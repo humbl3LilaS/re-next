d8bb22e166ae7e19f45c20487f65332bc21f2441
humbl3LilaS <sabishinekobebe@gmail.com>
2024-11-27 20:55:51 +0630

    learn: how redirecting from next.config.ts work

---
+++ app/not-allowed/page.tsx
@@ -0,0 +1,11 @@
+const NotAllowedPage = () => {
+    return (
+        <main>
+            <section>
+                <h1>Not Allowed</h1>
+            </section>
+        </main>
+    );
+};
+
+export default NotAllowedPage;
---
+++ app/redirected-from-not-allowed/page.tsx
@@ -0,0 +1,13 @@
+const RedirectedPage = () => {
+    return (
+        <main>
+            <section>
+                <h1>
+                    Redirect from /not-allowed via next.config.js
+                </h1>
+            </section>
+        </main>
+    );
+};
+
+export default RedirectedPage;
